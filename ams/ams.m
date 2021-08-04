1f1932e47833421450bf4284fedcf4735cc4fdc1
imu-HaoLi <88430176+imu-HaoLi@users.noreply.github.com>
2021-08-04 17:24:01 +0800

    Add files via upload

---
+++ ams/ams.m
@@ -0,0 +1,21 @@
+function cepDpDD = my_features_ams(sub_gf)
+
+nChan = 64;
+
+%sub_gf = reshape(sub_gf, 1, length(sub_gf));
+sub_gf = sub_gf';
+
+nFrame = floor(length(sub_gf)/160)-1;
+
+ns_ams = extract_AMS_perChan(sub_gf,nChan,16e3);
+
+AMS_points = ns_ams';
+AMS_points = AMS_points(1:nFrame,:);
+
+AMS_points = AMS_points.';
+
+%AMS_points = [zeros(size(AMS_points,1),1) AMS_points];
+
+cepDpDD = AMS_points;
+
+end
---
+++ logmel/log_mel.m
@@ -0,0 +1,19 @@
+function cepDpDD = my_features_log_mel(Sequence)
+
+samples = Sequence;
+sr = 16000;
+dorasta = 1;
+modelorder = 12;
+
+pspectrum = powspec(samples, sr, 0.02, 0.01); %winsize 320, stepsize 160
+
+% next group to critical bands
+aspectrum = audspec(pspectrum, sr , 40 , 'mel');
+nbands = size(aspectrum,1);
+
+% put in log domain
+cep = log10(aspectrum);
+
+
+    cepDpDD = cep;
+end
